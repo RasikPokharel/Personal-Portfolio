a34ff945964664d4cf2cca07de7cd76be214df00
Rasuckmaduck <rasik2058@gmail.com>
2021-04-01 15:40:42 -0400

    added app.js

---
+++ my-app/src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+import React, { useState, useEffect } from "react";
+import Navbar from "./Components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Container from 'react-bootstrap/esm/Container';
+
+function App() {
+  const [load, upadateLoad] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+  }, []);
+  return (
+    <Router>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navbar />
+        <ScrollToTop />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/project" component={Projects} />
+          <Route path="/about" component={About} />
+          <Route path="/resume" component={Resume} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
